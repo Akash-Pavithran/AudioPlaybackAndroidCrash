09878f2df50f133df07663281b38400a387dfaf6
Akash <akashakash38@gmail.com>
2025-02-26 22:34:18 +0530

    crash reproducible in android

--- App.tsx
+++ App.tsx
@@ -6,113 +6,113 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button, SafeAreaView, Text} from 'react-native';
+import {AudioManager, StreamState} from 'react-native-audio-playback';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const audios = {
+  MAIN_MENU_BGM: require('./assets/sounds/mainMenu_bgm_44100_2.mp3'),
+  SIX_HIT: require('./assets/sounds/six_hit_ball_sound.mp3'), // <--- Loading this audio crashes android
+};
+console.log('audios', audios);
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [streamState, setStreamState] = React.useState<StreamState>();
+  const streamStatus = AudioManager.shared.getStreamState();
+  const [audioPlayers, setAudioPlayers] = React.useState({});
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  console.log('streamStatus', streamStatus, streamState);
+
+  const getStreamState = () => {
+    const state = AudioManager.shared.getStreamState();
+    return state;
+  };
+  const setupStream = () => {
+    const state = getStreamState();
+    console.log('state in setupStream fn', state);
+
+    if (state === StreamState.closed) {
+      AudioManager.shared.setupAudioStream();
+    }
+    setStreamState(state);
   };
 
+  const openStream = () => {
+    const state = getStreamState();
+    console.log('state in openStream fn', state);
+
+    if (state !== StreamState.open) {
+      AudioManager.shared.openAudioStream();
+      setStreamState(state);
+    }
+  };
+
+  const pauseStream = () => {
+    const state = getStreamState();
+    console.log('state in pauseStream fn', state);
+
+    if (state !== StreamState.paused) {
+      AudioManager.shared.pauseAudioStream();
+    }
+  };
+
+  const loadSounds = async () => {
+    const menuMusic = await AudioManager.shared.loadSound(audios.MAIN_MENU_BGM);
+
+    const sixHit = await AudioManager.shared.loadSound(audios.SIX_HIT);
+
+    setAudioPlayers({
+      menuMusic: menuMusic,
+      sixHit: sixHit,
+    });
+  };
+  console.log({audioPlayers});
+
+  const play1 = () => {
+    audioPlayers?.menuMusic?.loopSound(false);
+    audioPlayers?.menuMusic?.playSound();
+  };
+
+  const pause1 = () => {
+    audioPlayers?.menuMusic?.pauseSound();
+  };
+
+  const play2 = () => {
+    audioPlayers?.sixHit?.loopSound(false);
+    audioPlayers?.sixHit?.playSound();
+  };
+  const pause2 = () => {
+    audioPlayers?.sixHit?.pauseSound();
+  };
+  const closeStream = () => {
+    const state = getStreamState();
+    console.log('state in closeStream fn', state);
+
+    if (state !== StreamState.closed) {
+      AudioManager.shared.closeAudioStream();
+    }
+  };
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+      }}>
+      <Text>{streamState}</Text>
+
+      <Button title="setup stream" onPress={setupStream} />
+      <Button title="open stream" onPress={openStream} />
+      <Button title="Pause" onPress={pauseStream} />
+      <Button title="Load Sounds" onPress={loadSounds} />
+      <Button title="Play1" onPress={play1} />
+      <Button title="Pause1" onPress={pause1} />
+      <Button title="Play2" onPress={play2} />
+      <Button title="Pause2" onPress={pause2} />
+      <Button title="close stream" onPress={closeStream} />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
